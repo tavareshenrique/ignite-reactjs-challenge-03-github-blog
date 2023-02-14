c4d2da8124d1557266d19856136e87569abbb14f
Henrique Tavares <ihenrits@gmail.com>
2023-02-14 20:57:57 -0300

    Update DefaultLayout styles to include 15rem bottom margin.

--- src/layout/DefaultLayout/styles.ts
+++ src/layout/DefaultLayout/styles.ts
@@ -2,8 +2,7 @@ import styled from 'styled-components';
 
 export const Content = styled.div`
   max-width: 54rem;
-  height: calc(100vh - 10rem);
-  margin: 0 auto;
+  margin: 0 auto 15rem auto;
   padding: 2.5rem;
   display: flex;
   flex-direction: column;
