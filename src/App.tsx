b25a9bf45d01d55a6f1a9cf759c6104d90f944ff
Henrique Tavares <ihenrits@gmail.com>
2023-02-13 22:04:51 -0300

    configuring global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import { ThemeProvider } from 'styled-components';
+
+import { Header } from './components/Header';
+
+import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
+
 export function App() {
-  return <h1>Github Blog</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
+      <Header />
+    </ThemeProvider>
+  );
 }
