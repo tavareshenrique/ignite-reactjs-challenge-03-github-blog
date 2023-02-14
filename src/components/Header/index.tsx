df00d3efecafad4221a29c41f1896b8b67072654
Henrique Tavares <ihenrits@gmail.com>
2023-02-14 20:32:47 -0300

    Add aria-hidden and tabIndex attributes to Header images.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,10 +4,10 @@ export function Header() {
   return (
     <HeaderContainer>
       <div>
-        <img src="/left-effect.png" alt="" />
+        <img src="/left-effect.png" alt="" aria-hidden="true" tabIndex={-1} />
       </div>
       <div>
-        <img src="/right-effect.png" alt="" />
+        <img src="/right-effect.png" alt="" aria-hidden="true" tabIndex={-1} />
       </div>
     </HeaderContainer>
   );
