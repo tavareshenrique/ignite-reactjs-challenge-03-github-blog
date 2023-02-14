483b0faae17bd156d32eb76fe2853b9fa46bfa2e
Henrique Tavares <ihenrits@gmail.com>
2023-02-14 20:39:27 -0300

    Add Content wrapper for DefaultLayout components

--- src/layout/DefaultLayout/index.tsx
+++ src/layout/DefaultLayout/index.tsx
@@ -2,12 +2,16 @@ import { Outlet } from 'react-router-dom';
 
 import { Header } from '../../components';
 
+import { Content } from './styles';
+
 export default function DefaultLayout() {
   return (
     <>
       <Header />
 
-      <Outlet />
+      <Content>
+        <Outlet />
+      </Content>
     </>
   );
 }
---
+++ src/layout/DefaultLayout/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Content = styled.div`
+  max-width: 54rem;
+  height: calc(100vh - 10rem);
+  margin: 0 auto;
+  padding: 2.5rem;
+  display: flex;
+  flex-direction: column;
+`;
