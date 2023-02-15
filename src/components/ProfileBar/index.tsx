ecb4ee762422fafda17017c17492d64142a7c408
Henrique Tavares <ihenrits@gmail.com>
2023-02-14 21:42:41 -0300

    Add styles for ProfileBar component, including ProfileInfoContent, ProfileInfoGroup, Link, ProfileName, and ProfileDescription.

--- src/components/ProfileBar/index.tsx
+++ src/components/ProfileBar/index.tsx
@@ -1,6 +1,15 @@
 import * as Avatar from '@radix-ui/react-avatar';
 
-import { ProfileBarContainer } from './styles';
+import { FaExternalLinkAlt } from 'react-icons/fa';
+
+import {
+  ProfileBarContainer,
+  ProfileDescription,
+  ProfileInfoContent,
+  ProfileInfoGroup,
+  ProfileName,
+  Link,
+} from './styles';
 
 export function ProfileBar() {
   return (
@@ -12,6 +21,26 @@ export function ProfileBar() {
           alt="Provavelmente é a foto de uma pessoa."
         />
       </Avatar.Root>
+
+      <ProfileInfoContent>
+        <ProfileInfoGroup>
+          <Link
+            href="/profile"
+            role="link"
+            aria-label="Acesse ao perfil do Github do Henrique Tavares."
+          >
+            Github
+            <FaExternalLinkAlt size={12} />
+          </Link>
+
+          <ProfileName>Henrique Tavares</ProfileName>
+        </ProfileInfoGroup>
+        <ProfileDescription>
+          Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
+          viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat
+          pulvinar vel mass.
+        </ProfileDescription>
+      </ProfileInfoContent>
     </ProfileBarContainer>
   );
 }
--- src/components/ProfileBar/styles.ts
+++ src/components/ProfileBar/styles.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
 export const ProfileBarContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
   width: 100%;
   height: 13rem;
 
@@ -11,12 +15,15 @@ export const ProfileBarContainer = styled.div`
   border-radius: 10px;
 
   .avatar-container {
+    flex: 1;
+
     display: inline-flex;
     align-items: center;
     justify-content: center;
     vertical-align: middle;
     overflow: hidden;
     user-select: none;
+    max-width: 148px;
     width: 148px;
     height: 148px;
     border-radius: 8px;
@@ -28,4 +35,52 @@ export const ProfileBarContainer = styled.div`
       border-radius: inherit;
     }
   }
+`;
+
+export const ProfileInfoContent = styled.div`
+  flex: 2;
+
+  margin-left: 34px;
+`;
+
+export const ProfileInfoGroup = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Link = styled.a`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  color: ${({ theme }) => theme.blue};
+
+  text-decoration: none;
+
+  svg {
+    margin-left: 8px;
+    color: ${({ theme }) => theme.blue};
+  }
+
+  &:hover {
+    border-bottom: 1px solid ${({ theme }) => theme.blue};
+  }
+`;
+
+export const ProfileName = styled.span`
+  font-size: 1.5rem;
+  line-height: 130%;
+  font-weight: bold;
+  color: ${({ theme }) => theme['base-title']};
+`;
+
+export const ProfileDescription = styled.p`
+  margin-top: 8px;
+
+  font-size: 1rem;
+  line-height: 160%;
+  font-weight: 400;
+  color: ${({ theme }) => theme['base-text']};
 `;
