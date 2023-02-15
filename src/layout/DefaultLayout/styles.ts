eee31b744d5986c81b4db18b26851bfd31d16fa6
Henrique Tavares <ihenrits@gmail.com>
2023-02-14 21:42:26 -0300

    Update max-width of DefaultLayout content to 60rem.

--- src/layout/DefaultLayout/styles.ts
+++ src/layout/DefaultLayout/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Content = styled.div`
-  max-width: 54rem;
+  max-width: 60rem;
   margin: 0 auto 15rem auto;
   padding: 2.5rem;
   display: flex;
