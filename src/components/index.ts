76826f28c2e2d37bfd148ef1bc3f807bff3ac35e
Henrique Tavares <ihenrits@gmail.com>
2023-03-16 12:06:46 -0300

    Add Link and Badge components to index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@ export { Header } from './Header';
 export { ProfileBar } from './ProfileBar';
 export { SearchBar } from './SearchBar';
 export { Card } from './Card';
+export { Link } from './Link';
+export * as Badge from './Badge';
