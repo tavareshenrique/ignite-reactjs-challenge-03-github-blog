39cd29517bf365b2eda6de72d4e40e18ad576e62
Henrique Tavares <ihenrits@gmail.com>
2023-04-15 20:34:23 -0300

    make a reques for user data in github

---
+++ src/components/ProfileBar/@interfaces.ts
@@ -0,0 +1,9 @@
+export interface IGithubApiAvatar {
+  avatar_url: string;
+  bio: string;
+  company: string;
+  followers: number;
+  html_url: string;
+  login: string;
+  name: string;
+}
--- src/components/ProfileBar/index.tsx
+++ src/components/ProfileBar/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import * as Avatar from '@radix-ui/react-avatar';
 
 import {
@@ -6,6 +8,8 @@ import {
   FaBuilding,
   FaUserFriends,
 } from 'react-icons/fa';
+
+import api from '../../services/api';
 
 import { Badge, Link } from '../';
 
@@ -16,14 +20,23 @@ import {
   ProfileInfoGroup,
   ProfileName,
 } from './styles';
+import { IGithubApiAvatar } from './@interfaces';
 
 export function ProfileBar() {
+  const [user, setUser] = useState<IGithubApiAvatar>({} as IGithubApiAvatar);
+
+  useEffect(() => {
+    api.get<IGithubApiAvatar>('/users/tavareshenrique').then((response) => {
+      setUser(response.data);
+    });
+  }, []);
+
   return (
     <ProfileBarContainer>
       <Avatar.Root className="avatar-container">
         <Avatar.Image
           className="avatar-container__image"
-          src="http://github.com/tavareshenrique.png"
+          src={`http://github.com/${user.login}.png`}
           alt="Provavelmente é a foto de uma pessoa."
         />
       </Avatar.Root>
@@ -31,37 +44,33 @@ export function ProfileBar() {
       <ProfileInfoContent>
         <ProfileInfoGroup>
           <Link
-            href="https://github.com.br/tavareshenrique"
+            href={user.html_url}
             role="link"
-            aria-label="Acesse ao perfil do Github do Henrique Tavares."
+            aria-label={`Acesse ao perfil do Github do ${user.name}.`}
           >
             Github
             <FaExternalLinkAlt size={12} />
           </Link>
 
-          <ProfileName>Henrique Tavares</ProfileName>
+          <ProfileName>{user.name}</ProfileName>
         </ProfileInfoGroup>
-        <ProfileDescription>
-          Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
-          viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat
-          pulvinar vel mass.
-        </ProfileDescription>
+        <ProfileDescription>{user.bio}</ProfileDescription>
 
         <Badge.Root>
           <Badge.Badge
-            ariaLabel="Nome de usuário no Github do Henrique Tavares."
+            ariaLabel={`Nome de usuário do Github do ${user.name}.`}
             icon={<FaGithub size={16} />}
-            text="tavareshenrique"
+            text={user.login}
           />
           <Badge.Badge
-            ariaLabel="Nome de organização que o Henrique Tavares trabalha."
+            ariaLabel={`Empresa onde o ${user.name} trabalha.`}
             icon={<FaBuilding size={16} />}
-            text="Getninjas"
+            text={user.company}
           />
           <Badge.Badge
-            ariaLabel="Número de seguidores do Henrique Tavares no Github."
+            ariaLabel={`Quantidade de seguidores do ${user.name}.`}
             icon={<FaUserFriends size={16} />}
-            text="32 seguidores"
+            text={`${user.followers} seguidores`}
           />
         </Badge.Root>
       </ProfileInfoContent>
